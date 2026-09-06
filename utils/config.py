8dbcf6b71ff70e07ca9dd3cc6d119613a41e1330
justinbeatdown <53517512+justinbeatdown@users.noreply.github.com>
2026-09-06 18:02:47 -0400

    Bump Pitmark Cloud to v0.21.35

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-PITMARK_RELEASE_VERSION = "0.21.34"
+PITMARK_RELEASE_VERSION = "0.21.35"
 
 
 class Settings(BaseSettings):
