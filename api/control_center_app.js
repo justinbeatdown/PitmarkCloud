cdad2d72c58debaca9b7e055efa27aee91439600
justinbeatdown <53517512+justinbeatdown@users.noreply.github.com>
2026-09-18 19:05:37 -0400

    Refresh Control Center views module

--- api/control_center_app.js
+++ api/control_center_app.js
@@ -1,5 +1,5 @@
 import { api, abortScope, clearCache } from './control-center-api.js';
-import { DOMAIN_META, renderDomain } from './control-center-views.js';
+import { DOMAIN_META, renderDomain } from './control-center-views.js?v=20260918metrics1';
 
 const VALID_DOMAINS = new Set(Object.keys(DOMAIN_META));
 const root = document.getElementById('pitmark-control');
