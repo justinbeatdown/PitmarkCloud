f1c10291a35ac0a648a4c76c5159525b08ed67a2
justinbeatdown <53517512+justinbeatdown@users.noreply.github.com>
2026-09-18 18:53:50 -0400

    Add Astra self-test flag and bump Cloud version

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,7 @@ from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-PITMARK_RELEASE_VERSION = "0.21.45"
+PITMARK_RELEASE_VERSION = "0.21.46"
 
 
 class Settings(BaseSettings):
